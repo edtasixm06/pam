87123a2bea52ef9a17740d29d1aa53290773f000
edtasixm06 <asixm06@edt.org>
2018-11-22 21:46:09 +0100

    mount

--- hostpam:18mount/auth.sh
+++ hostpam:18mount/auth.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # @edt ASIX-M06
 # -----------------
-authconfig  --enableshadow --enablelocauthorize --enableldap  --ldapserver='ldap' --ldapbase='dc=escoladeltreball,dc=org' --enableldapauth  --updateall
+authconfig  --enableshadow --enablelocauthorize --enableldap  --ldapserver='ldap' --ldapbase='dc=edt,dc=org' --enableldapauth  --updateall
 
 
 
